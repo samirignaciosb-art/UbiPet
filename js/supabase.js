f58c44b43f1eb1f5af8a7ce8084af28f3c634d0b
samirignaciosb-art <samirignaciosb@gmail.com>
2026-02-20 20:41:32 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,6 @@
+const supabaseClient = supabase.createClient(
+  "https://exeeqykieytuvlzdbsnn.supabase.co",
+  "sb_publishable_ffBzZEwygXXuyMDNDWVVoA_qxExK9bl"
+);
 
+export default supabaseClient;

f5933736fabde8b981a366e2312b9caef3cb2374
samirignaciosb-art <samirignaciosb@gmail.com>
2026-02-20 20:42:38 -0300

    Create perfiles.js

---
+++ js/perfiles.js
@@ -0,0 +1,9 @@
+import supabaseClient from "./supabase.js";
+
+export async function guardarPerfil() {
+  // ... tu lógica de guardar perfil aquí
+}
+
+export async function cargarPerfil() {
+  // ... tu lógica de cargar perfil aquí
+}

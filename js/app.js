14b260d781df5dae61a218bc945e7d25486a227c
samirignaciosb-art <samirignaciosb@gmail.com>
2026-02-20 21:21:53 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
-import './login.js';
+import { login, registrar } from './login.js';
 import './main.js';
 import './perfiles.js';
 import './qr.js';
 import './rescate.js';
 import './supabase.js';
+
+// ✅ Exponer funciones al HTML
+window.login = login;
+window.registrar = registrar;

cf5ff62b2f64793b64b37b5db1c3dd69fd9f13dc
samirignaciosb-art <samirignaciosb@gmail.com>
2026-02-20 21:55:17 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,21 @@
-import { guardarPerfil, generarQR, cerrarSesion, copiarURL } from './perfil.js';
 import { login, registrar } from './login.js';
+import { guardarPerfil, generarQR, cerrarSesion, copiarURL, togglePerdida } from './perfil.js';
+import { contactarDueno, enviarCorreo, copiarTelefono, enviarUbicacion } from './rescate.js';
 import './main.js';
 import './qr.js';
-import './rescate.js';
 import './supabase.js';
 
 // ✅ Exponer funciones al navegador
 window.login = login;
 window.registrar = registrar;
+
 window.guardarPerfil = guardarPerfil;
 window.generarQR = generarQR;
 window.cerrarSesion = cerrarSesion;
 window.copiarURL = copiarURL;
+window.togglePerdida = togglePerdida;
+
+window.contactarDueno = contactarDueno;
+window.enviarCorreo = enviarCorreo;
+window.copiarTelefono = copiarTelefono;
+window.enviarUbicacion = enviarUbicacion;

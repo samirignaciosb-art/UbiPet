30031288b2d17d7c8134785c56b61d54cc658039
samirignaciosb-art <samirignaciosb@gmail.com>
2026-02-20 21:00:19 -0300

    Delete js directory

--- js/supabase.js
+++
@@ -1,6 +0,0 @@
-const supabaseClient = supabase.createClient(
-  "https://exeeqykieytuvlzdbsnn.supabase.co",
-  "sb_publishable_ffBzZEwygXXuyMDNDWVVoA_qxExK9bl"
-);
-
-export default supabaseClient;

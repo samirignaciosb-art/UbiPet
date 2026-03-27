8d65500dd6e16d628b05df7a16979d55ee50a5f0
samirignaciosb-art <samirignaciosb@gmail.com>
2026-03-27 15:29:31 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js
 importScripts('https://www.gstatic.com/firebasejs/10.12.0/firebase-messaging-compat.js')
 
 firebase.initializeApp({
-  apiKey:            'AIzaSyC2RYcFsBbmippii29V2LKTjCE7OY6yuy0',
+  apiKey:            'AIzaSyC9Qi0GjiNA1drGgRwyKxRbqUyRjzlaAtQ',
   authDomain:        'ubipet1push.firebaseapp.com',
   projectId:         'ubipet1push',
   storageBucket:     'ubipet1push.firebasestorage.app',
